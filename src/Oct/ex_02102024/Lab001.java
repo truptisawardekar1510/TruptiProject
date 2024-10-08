d8f404c59a4b4693d5855449c80e20685a9158f6
truptisawardekar1510 <truptisawardekar1510.com>
2024-10-08 22:51:54 +0530

    Test

--- src/Oct/ex_02102024/Lab001.java
+++ src/Oct/ex_02102024/Lab001.java
@@ -1,4 +1,10 @@
 package Oct.ex_02102024;
 
 public class Lab001 {
+    // main Method
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
+
+// Output - Hello World!
---
+++ src/Oct/ex_02102024/test.java
@@ -0,0 +1,8 @@
+package Oct.ex_02102024;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+    }
+}
